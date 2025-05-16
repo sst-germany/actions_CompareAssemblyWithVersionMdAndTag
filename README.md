b9f2a972e3d296933dd24266b82b217ffa1e6890
uwekli <uwe.klingelhoeller@sst-germany.de>
2025-05-16 10:14:25 +0200

    Initial

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # actions_CompareAssemblyWithVersionMdAndTag
+
+This action reads the version number from the provided AssemblyPath and 
+compares it with the version number in the VERSION.md file and the Git tag.
+The Git tag must adhere to the schema 'releases/v{assembly-version}'.
+
+The action is executed on a windows powershell.
---
+++ main.ps1
@@ -0,0 +1,40 @@
+param(
+  [string]$AssemblyPath
+)
+
+if (-not (Test-Path $AssemblyPath)) {
+    Write-Error "Version-Assembly not found: $AssemblyPath"
+    exit 1
+}
+
+# Read the version from the assembly
+$assembly = [System.Reflection.AssemblyName]::GetAssemblyName($AssemblyPath)
+$assemblyVersion = $assembly.Version.ToString()
+Write-Host "Assembly: $assemblyVersion"
+
+# Get name of the Git-Tag
+$tagName = $env:GITHUB_REF_NAME
+Write-Host "Git-Tag: $tagName"
+
+# Get Version from VERSION.md
+$markdownVersion = Get-Content -Path "VERSION.md" | Select-Object -First 1
+Write-Host "VERSION.md: $markdownVersion"
+
+# Output
+"assembly_version=$assemblyVersion" | Out-File -FilePath $env:GITHUB_OUTPUT -Append
+
+# Versionen vergleichen
+if ($assemblyVersion -ne $markdownVersion) {
+    Write-Error "Assembly-Version does not match with VERSION.md: $assemblyVersion <=> VERSION.md: $version"
+    exit 1
+}
+
+if ($tagName -notlike "releases/v*") {
+    Write-Error "Git tag does not start with 'releases/v': $tagName"
+    exit 1
+}
+
+if ($tagName -notmatch "^releases/v$assemblyVersion$") {
+    Write-Error "Git tag does not match the expected format 'releases/v<AssemblyVersion>': $tagName"
+    exit 1
+}
